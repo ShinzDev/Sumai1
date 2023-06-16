a0a3d3d2641951654ebd8ea822dbbacb1ac9f755
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2023-06-16 15:41:47 +0100

    ui in  place

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,18 @@ import {logo} from '../assets';
 const Hero = () => {
   return (
     <header className=' w-full flex justify-center items-center flex-col'> 
-    <nav className='flex justify-between items-center flex-col'>
+    <nav className='flex justify-between items-center w-full mb-10 pt-3'>
       <img src={logo} alt="somz logo" className='w-28 object-contain'/>
+
+      <button type='button'
+      onClick={() => window.open('')} className='black_btn'>
+        Github
+      </button>
     </nav>
+    <h1 className='head_text'>
+      Summarize Articles with <br />
+      <span className='orange_gradient'>OpenAI GPT-4</span>
+    </h1>
     </header>
 
 
